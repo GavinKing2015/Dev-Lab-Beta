7e0b76b2313005178ba90810dcca81bd04c274dd
Whitehawk2015 <85392086+Whitehawk2015@users.noreply.github.com>
2021-09-05 19:21:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('<h3>Hello NodeJs on David Wiles Heroku</h3></br>);   
+    res.send('<h3>Hello NodeJs on David Wiles Heroku</h3></br>');   
 
 });
 
